a90a3efe41ae89e1e5780c840da254e59f90f2b5
Henrique <rigoni1912@gmail.com>
2023-10-10 12:56:01 -0300

    feat: add my tech Stack

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import { NavBar } from '@/components/NavBar'
 import './page.scss'
 import { Profile } from '@/components/Profile'
+import { Mytech } from '@/components/MyTech'
 
 export default function Home() {
   return (
     <main>
         <NavBar />
         <Profile />
+        <Mytech />
     </main>
   )
 }
---
+++ src/components/MyTech/index.js
@@ -0,0 +1,34 @@
+import "./styles.scss"
+import { FaReact, FaJsSquare, FaNode, FaSass, FaFigma, FaAngular, FaGitAlt, FaCss3Alt } from "react-icons/fa";
+import { BiLogoTypescript } from "react-icons/bi";
+import { SiMysql, SiStyledcomponents} from "react-icons/si";
+
+export function Mytech () {
+  return (
+      <div className="tech-container">
+        <h1>My tech Stack</h1>
+        <p>Technologies I’ve been working with recently</p>
+        <div className="my-tech">
+        <p>Technologies developed</p>
+        <div className="container-skill">
+        <a  title="react"><FaReact /></a>
+        <a  title="JavaScript"><FaJsSquare /></a>
+        <a  title="TypeScript"><BiLogoTypescript /></a>
+        <a  title="Sass"><FaSass /></a>
+        <a  title="CSS"><FaCss3Alt /></a>
+        <a  title="styled-components"><SiStyledcomponents /></a>
+        <a  title="React-Native"><FaReact /></a>
+        <a  title="Git"><FaGitAlt /></a>
+        </div>
+        <p>Technologies in development</p>
+        <div className="mid-tech">
+        <a  title="Node"><FaNode /></a>
+        <a  title="Angular"><FaAngular /></a>
+        <a  title="Mysql"><SiMysql /></a>
+        <a  title="Figma"><FaFigma /></a>
+        </div>
+        </div>
+      
+      </div>
+  )
+}

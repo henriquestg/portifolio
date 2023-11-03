0df75db6fceb3dbd44213b1c3f337db5da08450a
Henrique <rigoni1912@gmail.com>
2023-11-02 22:36:48 -0300

    att

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -32,6 +32,27 @@ export function Projects () {
         </div>
     </div>
     </div>
+
+    <div className="projects-preview">
+        <Image id="project-photo" src={Ecommerce} alt="foto projeto" />
+        <div className="projects">
+        <h2>My ecommerce</h2>
+        <p>Um site criado para ter conceito ecommerce para venda de móveis</p>
+        <p>Tech stack: Next.js React, Sass, JavaScript</p>
+        <div className="teck-links"> 
+        <div className="link"> 
+        <FaLink />
+          <a href="https://my-ecommerce-alpha-rust.vercel.app/" target="_blank">Live preview</a>
+        </div>
+        <div className="link"> 
+        <FaGithub />
+        <a href="https://github.com/henriquestg/my-ecommerce/tree/master" target="_blank">View code</a>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    
     </div>
     </section>
   )

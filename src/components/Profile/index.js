960590a9408ea406e90002bdf8077e1d8340c3df
Henrique <rigoni1912@gmail.com>
2023-11-30 19:55:08 -0300

    refact: title profile

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -11,7 +11,7 @@ export function Profile () {
       <h1>Hi👋, </h1>
       <h1> My name is </h1>
       <h1 id='text-color'> Henrique Rigoni </h1>
-      <h1> I build things for web</h1>
+      <h1> I'm a developer Front-End</h1>
       <a href='https://github.com/henriquestg' target="_blank"><FaGithub /></a> 
       <a href='https://www.linkedin.com/in/henrique-rigoni/' target="_blank"><FaLinkedin /></a> 
       <a href='https://www.instagram.com/henriquerigoni/' target="_blank"><FaInstagramSquare /></a> 
